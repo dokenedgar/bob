222ee14f653c55fbf42c0575a272a6483c8acf99
Doken Edgar <dokenedgarjr@gmail.com>
2025-04-03 15:31:19 +0100

    Solved the bob exercism challenge

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
 pub fn reply(message: &str) -> &str {
-    todo!("have Bob reply to the incoming message: {message}")
+    let mut response: &str = "";
+
+    if message.trim().ends_with("?") {
+        if message == message.trim().to_uppercase() && is_alphabetic(message) {
+            response = "Calm down, I know what I'm doing!";
+        } else {
+            response = "Sure."
+        }
+    } else if message == message.trim().to_uppercase()
+        && message.trim() != ""
+        && is_alphabetic(message)
+    {
+        response = "Whoa, chill out!";
+    } else if message.trim() == "" {
+        response = "Fine. Be that way!";
+    } else {
+        response = "Whatever.";
+    }
+
+    response
+}
+
+fn is_alphabetic(s: &str) -> bool {
+    let mut has_alphabet = false;
+    s.chars().for_each(|x| {
+        if x.is_alphabetic() {
+            has_alphabet = true
+        }
+    });
+    has_alphabet
 }
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let res = bob::reply("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!");
+    println!("Result: {res}");
+}
